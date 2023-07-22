9d87e13e8401d7d802e74561b0987931b8a37ad1
Елена <lenagolubeva96@gmail.com>
2023-07-22 18:41:15 +0300

    добавлена задача 2

--- 3/script.js
+++ 3/script.js
@@ -12,6 +12,15 @@
 // Пользователь вводит с клавиатуры число, если ввёл текст, необходимо вывести что значение задано неверно
 // Создать фукнцию, которая высчитывает 13% от данного числа и выводит в консоль текст "Размер заработной платы за вычетом налогов равен значение"
 
+function salary (number) {
+    return number*0.87;
+}
+
+let number = Number(prompt('Введите число'));
+if (typeof number === 'number') {
+    console.log(`Размер заработной платы за вычетом налогов равен ${salary(number)}`);
+}
+else console.log('Вы ввели неверное значение');
 
 
 // Задание 3
@@ -37,26 +46,26 @@
 // 4. Деление
 // Необходимо сделать так, чтобы функция вернула число, например выражение console.log(sum(2, 6)); должно вывести число 8 в консоль (sum - функция сложения в данном примере, ваши названия функций могут отличаться). Округлять значения, которые возвращают функции не нужно, однако, обратите внимание на разность, функция должна вычесть из большего числа меньшее, либо вернуть 0, если числа равны. Функциям всегда передаются корректные числа, проверки на NaN, Infinity делать не нужно.
 
-function sum(number1, number2) {
-    return number1+number2;
-}
+// function sum(number1, number2) {
+//     return number1+number2;
+// }
 
-function difference(number1, number2) {
-    if (number1 > number2) return number1-number2;
-    else return number2-number1;
-}
+// function difference(number1, number2) {
+//     if (number1 > number2) return number1-number2;
+//     else return number2-number1;
+// }
 
-function multiplication(number1, number2) {
-    return number1*number2;
-}   
+// function multiplication(number1, number2) {
+//     return number1*number2;
+// }   
 
-function division(number1, number2) {
-    return number1/number2;
-}
+// function division(number1, number2) {
+//     return number1/number2;
+// }
 
-let number1 = Number(prompt('Введите первое число'));
-let number2 = Number(prompt('Введите второе число'));
-console.log(`Сумма чисел равна ${sum(number1, number2)}`);
-console.log(`Разность чисел равна ${difference(number1, number2)}`);
-console.log(`Произведение чисел равно ${multiplication(number1, number2)}`);
-console.log(`Частное чисел равно ${division(number1, number2)}`);
+// let number1 = Number(prompt('Введите первое число'));
+// let number2 = Number(prompt('Введите второе число'));
+// console.log(`Сумма чисел равна ${sum(number1, number2)}`);
+// console.log(`Разность чисел равна ${difference(number1, number2)}`);
+// console.log(`Произведение чисел равно ${multiplication(number1, number2)}`);
+// console.log(`Частное чисел равно ${division(number1, number2)}`);
